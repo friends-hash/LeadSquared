96465cac0f992054ccf759634aad155ec3ab91f3
sonalimahajan98 <sonaliksonawane098@gmail.com>
2026-03-10 11:41:33 +0530

    Added frontend and backend setup

---
+++ frontend/src/Components/sidebar/sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>Dashboard</li>
+        <li>Reports</li>
+        <li>Leads</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
